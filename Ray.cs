ff09bfb2bf357d09bedfb968feb8f3ad6cb85bd9
Matt Wrench <matt@mattwrench.com>
2021-01-24 12:25:27 -0600

    Add Ray class

---
+++ Ray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RayTracer
+{
+    class Ray
+    {
+        public Vector3 Origin;
+        public Vector3 Direction;
+
+        public Ray()
+        {
+            Origin = new Vector3();
+            Direction = new Vector3();
+        }
+
+        public Ray(Vector3 origin, Vector3 direction)
+        {
+            Origin = origin;
+            Direction = direction;
+        }
+
+        Vector3 At(double t)
+        {
+            return Origin.Add(Direction.Multiply(t));
+        }
+    }
+}
