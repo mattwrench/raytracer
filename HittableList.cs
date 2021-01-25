9f910cf41e9d169a48b3081aec9e4903c30fc1cc
Matt Wrench <matt@mattwrench.com>
2021-01-24 19:17:43 -0600

    Add HittableList class

---
+++ HittableList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RayTracer
+{
+    class HittableList : Hittable
+    {
+        public List<Hittable> HittableObjects;
+
+        public HittableList()
+        {
+            HittableObjects = new List<Hittable>();
+        }
+
+        public void Add(Hittable hittable)
+        {
+            HittableObjects.Add(hittable);
+        }
+
+        // Checks for collisions with all hittable objects
+        public bool Hit(Ray r, double tMin, double tMax, HitRecord record)
+        {
+            HitRecord tempRecord = new HitRecord();
+            bool hitAnything = false;
+            double closestSoFar = tMax;
+
+            foreach (Hittable hittable in HittableObjects)
+            {
+                if (hittable.Hit(r, tMin, closestSoFar, tempRecord))
+                {
+                    hitAnything = true;
+                    closestSoFar = tempRecord.T;
+                    record = tempRecord;
+                }
+            }
+
+            return hitAnything;
+        }
+    }
+}
